a86dd14bdecbab5cc886cf2b210c7d08aa6558aa
Duncan Andrew <duncanandrew@live.com>
2017-03-30 17:32:27 -0700

    Initial Commit

---
+++ SimpleCalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleCalc
+//
+//  Created by Duncan Andrew on 3/30/17.
+//  Copyright © 2017 Duncan Andrew. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
