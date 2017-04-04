2853bfd77d60d8c2be179242e5d701f00b41781e
Duncan Andrew <duncanandrew@live.com>
2017-04-04 14:15:08 -0700

    added double support

--- SimpleCalc/main.swift
+++ SimpleCalc/main.swift
@@ -18,12 +18,12 @@ if(responseArray.count > 1){
     
     // Assign needed variables
     var operation = responseArray[responseArray.count - 1]
-    var numbers = [Int]()
+    var numbers = [Double]()
     var numbersValid = true
     
     // Convert strings to numbers and check their validity
     for i in 0...(responseArray.count-2){
-        var number = Int(responseArray[i])
+        var number = Double(responseArray[i])
         if number != nil{
             numbers.append(number!)
         }else{
@@ -36,7 +36,7 @@ if(responseArray.count > 1){
     if(numbersValid){
         if(operation == "count"){
             
-            var result = 0
+            var result = 0.0
             for i in 0...(numbers.count-1){
                 result = result + numbers[i]
             }
@@ -46,26 +46,26 @@ if(responseArray.count > 1){
             
             var sum = 0.0
             for i in 0...(numbers.count-1){
-                sum = sum + Double(numbers[i])
+                sum = sum + numbers[i]
             }
             var result = sum / Double(numbers.count)
             print("Result: \(result)")
             
         }else if(operation == "fact"){
             
-            if numbers.count <= 1 && numbers[0] >= 0{
+            if numbers.count <= 1 && numbers[0] >= 0 && floor(numbers[0]) == numbers[0]{
                 var result = 0
                 if numbers[0] == 0{
                     result = 0
                 }else{
                     result = 1
-                    for i in 1...numbers[0]{
+                    for i in 1...Int(numbers[0]){
                         result = result * i
                     }
                 }
                 print("Result: \(result)")
             }else {
-                print("Error: fact can only be called on one positive number")
+                print("Error: fact can only be called on one positive integer")
             }
             
         }else{
@@ -78,9 +78,9 @@ if(responseArray.count > 1){
     // Multi line operation
     
     // Assign needed variables
-    var number1 = Int(response)
+    var number1 = Double(response)
     var operation = readLine(strippingNewline: true)!
-    var number2 = Int(readLine(strippingNewline: true)!)
+    var number2 = Double(readLine(strippingNewline: true)!)
     
     // Do the calculation if the numbers are valid
     if number1 != nil && number2 != nil {
@@ -97,8 +97,12 @@ if(responseArray.count > 1){
             var result = Double(number1!) / Double(number2!)
             print("Result: \(result)")
         }else if(operation == "%"){
-            var result = number1! % number2!
-            print("Result: \(result)")
+            if floor(number1!) == number1! && floor(number2!) == number2!{
+                var result = Int(number1!) % Int(number2!)
+                print("Result: \(result)")
+            }else{
+               print("Error: % can only be performed on intagers")
+            }
         }else {
             print("Error: \(operation) is not a supported opperation")
         }
